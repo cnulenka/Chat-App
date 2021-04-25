3d4442ecb61ce1e352ba178343373259efd99945
cnulenka <sinulenka001@gmail.com>
2021-04-25 20:36:30 +0530

    setup channels

--- src/ChatApp/settings.py
+++ src/ChatApp/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'channels',
     'chat',
 ]
 
@@ -69,6 +70,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'ChatApp.wsgi.application'
+ASGI_APPLICATION = 'ChatApp.routing.application'
 
 
 # Database
